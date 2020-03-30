c73a9d3b575eead130fb6ad378bd0bea256d721f
UkhovArty <uhov.aa@phystech.edu>
2020-03-31 01:41:53 +0300

    ProbableParticlesMaker modified in oder to return proper results

--- src/ru/mipt/ProbableParticlesMaker.java
+++ src/ru/mipt/ProbableParticlesMaker.java
@@ -5,34 +5,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ProbableParticlesMaker {
-    private ArrayList<ArrayList<Particle>> allFstateCombinations;
-    private ArrayList<Decay> parsedDecays;
-    private HashMap<String,Particle> parsedParticles;
-
-
-    public ProbableParticlesMaker(ArrayList<ArrayList<Particle>> allFstateCombinations, ArrayList<Decay> parsedDecays, HashMap<String,Particle> parsedParticles) {
-        this.allFstateCombinations = allFstateCombinations;
-        this.parsedDecays = parsedDecays;
-        this.parsedParticles = parsedParticles;
-    }
-
-    public Map<Integer, ArrayList<Particle>> convertCombinationsToParticles() {
-        Map<Integer, ArrayList<Particle>> probableParticles = new HashMap<>();
-        for (ArrayList<Particle> fstateCombination : allFstateCombinations) {
-            for (Decay parsedDecay : parsedDecays) {
-                if (fstateCombination.equals(parsedDecay.particles)) {
-                    ArrayList<Particle> sameKeyParticles = new ArrayList<>();
-                    for (Particle particle : parsedParticles.values()) {
-                        if (parsedDecay.name.equals(particle.name) || parsedDecay.name.equals(particle.alias)) {
-                            sameKeyParticles.add(particle);
-                        }
-                    }
-                    System.out.println(parsedDecay.name);
-                    probableParticles.put(parsedDecay.particles.size(),sameKeyParticles);
-                }
-            }
-
-        }
-        return probableParticles;
-    }
+//    private HashMap<String, Decay> parsedDecays;
+//    private HashMap<String,Particle> parsedParticles;
+//
+//
+//    public ProbableParticlesMaker(HashMap<String, Decay> parsedDecays, HashMap<String,Particle> parsedParticles) {
+//        this.parsedDecays = parsedDecays;
+//        this.parsedParticles = parsedParticles;
+//    }
+//
+//    public Map<Particle, Decay> convertCombinationsToParticles(ArrayList<Particle> particles) {
+//        Map<Particle, Decay> probableParticles = new HashMap<>();
+//        for (String s : parsedDecays.keySet()) {
+//            String tmp = s.split(" ")[1];
+//            Integer counter = 0;
+//            if (Integer.parseInt(tmp) == particles.size()) {
+//                for (Particle particle : particles) {
+//                    if (s.contains(particle.name) || s.contains(particle.alias)) {
+//                        counter++;
+//                    }
+//                }
+//                if (counter == particles.size()) {
+//                    probableParticles.put(Decay.motherParticle, Decay);
+//                }
+//            }
+//        }
+//        return probableParticles;
+//    }
 }
