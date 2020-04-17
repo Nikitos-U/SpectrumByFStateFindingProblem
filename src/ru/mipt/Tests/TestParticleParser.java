10334e6a7431a6d3567487a2d115d25f9ca631ff
UkhovArty <uhov.aa@phystech.edu>
2020-04-17 14:48:37 +0300

    Tests for Parsers were made

--- src/ru/mipt/Tests/TestParticleParser.java
+++ src/ru/mipt/Tests/TestParticleParser.java
@@ -1,17 +1,30 @@
 package ru.mipt.Tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.mipt.Particle;
+import ru.mipt.ParticleParser;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class TestParticleParser {
 
     @Test
-    public void TestParticlesNumber() {
-
+    public void TestParticlesNumber() throws IOException {
+        ParticleParser particleParser = new ParticleParser();
+        HashMap<String, Particle> particles;
+        particles = particleParser.parse();
+        Assertions.assertEquals(971, particles.size());
     }
 
     @Test
-    public void TestForSomeParticlePresenceInTheList() {
-
+    public void TestForSomeParticlePresenceInTheList() throws IOException {
+        ParticleParser particleParser = new ParticleParser();
+        HashMap<String, Particle> particles;
+        particles = particleParser.parse();
+        Particle particle2 = particles.get("pi-");
+        Assertions.assertNotNull(particle2);
     }
 
 }
