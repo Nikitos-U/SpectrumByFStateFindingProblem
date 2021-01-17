60cabf97bea3d79f0558db8beb9a6f2ff874173e
UkhovArty <uhov.aa@phystech.edu>
2021-01-18 01:59:36 +0300

    test + results on system version with db

--- src/test/java/TimeMeasurementTest.java
+++ src/test/java/TimeMeasurementTest.java
@@ -11,7 +11,7 @@ import static ru.mipt.starters.SpectrumByFStateFinder.workingTime;
 
 
 public class TimeMeasurementTest {
-
+//
 //    @BeforeAll
 //    static void warmUp() {
 //        for (int i = 0; i < 5000; i++) {
@@ -22,14 +22,21 @@ public class TimeMeasurementTest {
     @SneakyThrows
     @Test
     public void measureWorkingTime() {
-        File csvOutputFile = new File("C:/Users/auhov/IdeaProjects/SpectrumByFStateFindingProblem/src/test/resources/timeMeasurementsResults.csv");
+        File csvOutputFile = new File("");
         PrintWriter pw = new PrintWriter(new FileOutputStream(csvOutputFile, true));
         pw.println("num of particles, working time ns");
         for (int i = 0; i < 10; i++) {
             main(new String[]{"pi+", "pi-"});
             pw.println(2 + ", " + workingTime);
         }
+        for (int i = 0; i < 10; i++) {
+            main(new String[]{"pi+", "pi-", "pi0"});
+            pw.println(3 + ", " + workingTime);
+        }
+//        for (int i = 0; i < 10; i++) {
+//            main(new String[]{"pi+", "pi-", "K+", "K-"});
+//            pw.println(4 + ", " + workingTime);
+//        }
         pw.close();
     }
-
 }
