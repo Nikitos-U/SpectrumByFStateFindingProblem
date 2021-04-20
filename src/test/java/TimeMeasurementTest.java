1bae341fefe1f8bb20142789f589db6546ee69a9
UkhovArty <uhov.aa@phystech.edu>
2021-04-21 02:12:09 +0300

    Speed Test

--- src/test/java/TimeMeasurementTest.java
+++ src/test/java/TimeMeasurementTest.java
@@ -1,88 +1,117 @@
-//import lombok.SneakyThrows;
-//import org.junit.jupiter.api.Test;
-//import ru.mipt.*;
-//import ru.mipt.parsers.DecayParser;
-//import ru.mipt.parsers.ParticleParser;
-//import ru.mipt.utils.DoubleKeyHashMap;
-//
-//import java.io.File;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//import java.util.HashMap;
-//
-//import static java.lang.System.nanoTime;
-//
-//public class TimeMeasurementTest {
-//    ParticleParser particleParser = new ParticleParser();
-//    HashMap<String, Particle> particles = particleParser.parse();
-//    DecayParser decayParser = new DecayParser(particles);
-//    DoubleKeyHashMap decays = decayParser.parse();
-//    Particle fake_mother_particle = new Particle("FAKE_MOTHER_PARTICLE_ADD_ALIAS", 42069.0, 0);
-//    ProbableParticlesMaker probableParticlesMaker = new ProbableParticlesMaker(decays);
-//    ParticleCombinator combinator = new ParticleCombinator(particles);
-//    DecaysFinder decaysFinder = new DecaysFinder(combinator, probableParticlesMaker);
-//
-//    public TimeMeasurementTest() throws IOException {
-//    }
-//
-//    @SneakyThrows
-//    public void warmUp() {
-//        particles.put("FAKE_MOTHER_PARTICLE_ADD_ALIAS", fake_mother_particle);
-//        Cascade fstate = new Cascade();
-//        for (String arg : new String[]{"pi+", "pi-"}) {
-//            for (Particle particle : particles.values()) {
-//                if (particle.getAliases().contains(arg)) {
-//                    fstate.getParticleList().add(particle);
-//                }
-//            }
-//        }
-//        System.out.println("started warm up");
-//        for (int i = 0; i < 5000; i++) {
-//            decaysFinder.findDecays(fstate);
-//        }
-//        System.out.println("ended warm up");
-//    }
-//
-//    @SneakyThrows
-//    @Test
-//    public void measureWorkingTime() {
-//        warmUp();
-//        System.out.println("started test");
-//        File csvOutputFile = new File("");
-//        PrintWriter pw = new PrintWriter(new FileOutputStream(csvOutputFile, true));
-//        pw.println("num of particles, working time ns");
-//        particles.put("FAKE_MOTHER_PARTICLE_ADD_ALIAS", fake_mother_particle);
-//        Cascade twoParticlesFstate = new Cascade();
-//        for (String arg : new String[]{"pi+", "pi-"}) {
-//            twoParticlesFstate.getParticleList().add(particles.get(arg));
-//        }
-//        for (int i = 0; i < 100; i++) {
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import ru.mipt.DecaysFinder;
+import ru.mipt.ParticleCombinator;
+import ru.mipt.ProbableParticlesMaker;
+import ru.mipt.dao.DecayRepository;
+import ru.mipt.dao.ParticleRepository;
+import ru.mipt.newAlgoEffort.FstateServiceImpl;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
+import static java.lang.System.nanoTime;
+
+
+public class TimeMeasurementTest {
+
+    ParticleRepository particleRepository;
+
+    DecayRepository decayRepository;
+
+    JdbcTemplate jdbcTemplate;
+
+    ObjectMapper mapper;
+
+    DecaysFinder finder;
+
+    ProbableParticlesMaker maker;
+
+    ParticleCombinator combinator;
+
+    FstateServiceImpl service;
+
+    private final String fstateTwoParticles = "K0,pi-";
+    private final String fstateThreeParticles = "K0,pi0,pi0";
+    private final String fstateFourParticles = "pi+,pi-,K0,K-";
+    private final String fstateFiveParticles = "pi+,pi+,pi-,K0,K-";
+
+
+    private void setUp() {
+        String NEO4J_URL = System.getenv("NEO4J_URL");
+        if (NEO4J_URL == null) NEO4J_URL = System.getProperty("NEO4J_URL", "jdbc:neo4j:http://localhost:7474/");
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(NEO4J_URL);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        mapper = new ObjectMapper();
+        mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+        mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+
+        decayRepository = new DecayRepository(mapper, jdbcTemplate);
+        particleRepository = new ParticleRepository(mapper, jdbcTemplate);
+
+        maker = new ProbableParticlesMaker(decayRepository);
+        combinator = new ParticleCombinator(particleRepository);
+
+        finder = new DecaysFinder(combinator, maker);
+
+        service = new FstateServiceImpl(particleRepository, finder);
+    }
+
+    @SneakyThrows
+    public void warmUp() {
+        setUp();
+        System.out.println("started warm up");
+        for (int i = 0; i < 1000; i++) {
+            Thread.sleep(10);
+            service.computeCascades(fstateTwoParticles);
+        }
+        System.out.println("ended warm up");
+    }
+
+    @SneakyThrows
+    @Test
+    public void measureWorkingTime() {
+        warmUp();
+        System.out.println("started test");
+        File csvOutputFile = new File("H2_measurements_Logs_off.csv");
+        PrintWriter pw = new PrintWriter(new FileOutputStream(csvOutputFile, true));
+
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(100);
+            long time = nanoTime();
+            service.computeCascades(fstateTwoParticles);
+            long timeOfRun = nanoTime() - time;
+            pw.println(2 + ", " + timeOfRun);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            Thread.sleep(10);
+            long time = nanoTime();
+            service.computeCascades(fstateThreeParticles);
+            long timeOfRun = nanoTime() - time;
+            pw.println(3 + ", " + timeOfRun);
+        }
+
+//        for (int i = 0; i < 10; i++) {
 //            long time = nanoTime();
-//            decaysFinder.findDecays(twoParticlesFstate);
-//            long timeOfRun = nanoTime() - time;
-//            pw.println(2 + ", " + timeOfRun);
-//        }
-//        Cascade threeParticlesFstate = new Cascade();
-//        for (String arg : new String[]{"pi+", "pi-", "pi0"}) {
-//            threeParticlesFstate.getParticleList().add(particles.get(arg));
-//        }
-//        for (int i = 0; i < 100; i++) {
-//            long time = nanoTime();
-//            decaysFinder.findDecays(threeParticlesFstate);
-//            long timeOfRun = nanoTime() - time;
-//            pw.println(3 + ", " + timeOfRun);
-//        }
-//        Cascade fourParticlesFstate = new Cascade();
-//        for (String arg : new String[]{"pi+", "pi-", "pi0", "K+"}) {
-//            fourParticlesFstate.getParticleList().add(particles.get(arg));
-//        }
-//        for (int i = 0; i < 100; i++) {
-//            long time = nanoTime();
-//            decaysFinder.findDecays(fourParticlesFstate);
+//            service.computeCascades(fstateFourParticles);
 //            long timeOfRun = nanoTime() - time;
 //            pw.println(4 + ", " + timeOfRun);
 //        }
-//        pw.close();
-//    }
-//}
+
+//        for (int i = 0; i < 100; i++) {
+//            long time = nanoTime();
+//            service.computeCascades(fstateFiveParticles);
+//            long timeOfRun = nanoTime() - time;
+//            pw.println(3 + ", " + timeOfRun);
+//        }
+
+        pw.close();
+    }
+}
